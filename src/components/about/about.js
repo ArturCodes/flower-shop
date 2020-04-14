8ef7ba6352436d56200b0f092f0c7792e7c7d120
Artur Shapiro <arturshapiro@gmail.com>
2020-04-14 00:50:05 -0500

    about page

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import './about.scss'
 import Footer from '../footer/footer'
+import Copyright from '../copyright/copyright'
+import LoremBlocks from '../lorem-blocks/lorem-blocks'
 
 const About = () => {
    return (
       <div className="about-wrapper">
-         <Footer/>
+         <div>
+            <div className="est">
+               <h1>Established in 2024</h1>
+               <LoremBlocks />
+               <br />
+               <LoremBlocks />
+               <br />
+               <LoremBlocks />
+            </div>
+         </div>
+
+         <Footer />
+         <Copyright />
       </div>
    )
 }
---
+++ src/components/lorem-blocks/lorem-blocks.js
@@ -0,0 +1,58 @@
+import React from "react"
+import './lorem.scss'
+
+const LoremBlocks = () => {
+   return (
+      <div>
+         <span className="greyed">Lorem</span>
+         <span className="greyed">ipsum</span>
+         <span className="greyed">dolor</span>
+         <span className="greyed">sit</span>
+         <span className="greyed">amet</span>
+         <span className="greyed">consectetur</span>
+         <span className="greyed">adipisicing</span>
+         <span className="greyed">elit</span>
+         <span className="greyed">Distinctio</span>
+         <span className="greyed">non</span>
+         <span className="greyed">sint</span>
+         <span className="greyed">dolor</span>
+         <br />
+         <span className="greyed">sunt</span>
+         <span className="greyed">dolore</span>
+         <span className="greyed">excepturi</span>
+         <span className="greyed">ducimus</span>
+         <span className="greyed">laborum</span>
+         <span className="greyed">earum</span>
+         <span className="greyed">eaque</span>
+         <span className="greyed">possimus</span>
+         <span className="greyed">alias</span>
+         <span className="greyed">minima</span>
+         <span className="greyed">aperiam</span>
+         <br />
+         <span className="greyed">eveniet</span>
+         <span className="greyed">provident</span>
+         <span className="greyed">commodi</span>
+         <span className="greyed">non</span>
+         <span className="greyed">totam</span>
+         <span className="greyed">corrupti</span>
+         <span className="greyed">dolore</span>
+         <span className="greyed">excepturi</span>
+         <span className="greyed">ducimus</span>
+         <span className="greyed">laborum</span>
+         <br />
+         <span className="greyed">earum</span>
+         <span className="greyed">eaque</span>
+         <span className="greyed">possimus</span>
+         <span className="greyed">sit</span>
+         <span className="greyed">amet</span>
+         <span className="greyed">consectetur</span>
+         <span className="greyed">adipisicing</span>
+         <span className="greyed">elit</span>
+         <span className="greyed">modi</span>
+         <span className="greyed">Consequuntur</span>
+         <span className="greyed">magni</span>
+      </div>
+   )
+}
+
+export default LoremBlocks
