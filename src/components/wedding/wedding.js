2bed7dec055bb0c6e2e62ad7023a7f4f2b7ec26b
Artur Shapiro <arturshapiro@gmail.com>
2020-04-13 23:17:01 -0500

    fixed

--- src/components/wedding/wedding.js
+++ src/components/wedding/wedding.js
@@ -7,7 +7,7 @@ const Wedding = () => {
             <div className="wed-info">
                <h2>Need Help With Wedding Decorations?</h2>
                <div>From setup to clean up, <br /> We got you covered</div>
-               <p>"I still can't believe that such a charming wedding could be produced. If it wasn't for Valleys Flora help, this wedding a lot less colorful" -Fiction Stranger Qoute</p>
+               <p>"I still can't believe that such a charming wedding could be produced. If it wasn't for Valleys Flora help, this wedding a lot less colorful" <br/><span>-Fiction Stranger Qoute</span></p>
             </div>
          <img src={require('../assets/wedding.jpg')} alt="wedding" />
       </div>
