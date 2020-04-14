e58a05d56225553e79ad8036a73d269c8bb9db70
Artur Shapiro <arturshapiro@gmail.com>
2020-04-13 23:04:06 -0500

    .

--- src/components/wedding/wedding.js
+++ src/components/wedding/wedding.js
@@ -4,11 +4,11 @@ import './wedding.scss'
 const Wedding = () => {
    return (
       <div className="wedding-wrapper">
-         <div className="wed-info">
-            <h2>Need Help With Wedding Decorations?</h2>
-            <div>From setup to clean up, <br/> We got you covered</div>
-            <p>"I couldn't imagine that such a wedding could exist. If it wasn't for Valleys Flora, this wedding a lot less colorful" -Fiction Stranger Qoute</p>
-         </div>
+            <div className="wed-info">
+               <h2>Need Help With Wedding Decorations?</h2>
+               <div>From setup to clean up, <br /> We got you covered</div>
+               <p>"I still can't believe that such a charming wedding could be produced. If it wasn't for Valleys Flora help, this wedding a lot less colorful" -Fiction Stranger Qoute</p>
+            </div>
          <img src={require('../assets/wedding.jpg')} alt="wedding" />
       </div>
    )
